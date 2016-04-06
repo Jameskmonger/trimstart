aa271790e771560424f02b5f0422dd229dd8bff7
James Monger <jameskmonger@hotmail.co.uk>
2016-04-06 23:54:47 +0100

    Implement trimstart

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function (str) {
-    
+    var index = 0;
+
+    while (str[index] === ' ') {
+        index++;
+    }
+
+    return str.substr(index, str.length);
 };
